9855584748d7afad55890c8ffdf94ff05ee700e0
Stoyan <stoyan.kostadinov85@gmail.com>
2023-09-21 00:56:52 +0300

    small css tweaks and message update

--- src/utils/gameUtils.js
+++ src/utils/gameUtils.js
@@ -3,7 +3,7 @@ import { findWinningLine } from "./winningLines";
 export const computeGameStatus = (squares, xIsNext, X_SYMBOL, O_SYMBOL) => {
   const { winner } = findWinningLine(squares) || {};
   if (winner) {
-    return `Looks like the ${winner} won this game!`;
+    return `Seems like the ${winner} won this game!`;
   }
   if (squares.every((square) => square)) {
     return "Draw Game! No one won this game.";
