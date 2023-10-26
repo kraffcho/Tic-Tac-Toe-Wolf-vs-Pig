e23fb53def25f367d0e76705f2b83b3b1b1bcba9
Nirmal Chamika <lynchamika@gmail.com>
2023-10-26 23:02:28 +0530

    +minmax bot added

--- src/utils/gameUtils.js
+++ src/utils/gameUtils.js
@@ -10,3 +10,24 @@ export const computeGameStatus = (squares, xIsNext, X_SYMBOL, O_SYMBOL) => {
   }
   return `Next move: ${xIsNext ? X_SYMBOL : O_SYMBOL}`;
 };
+
+export const WIN_STATE_POINTS = {
+  WON: 2,
+  LOOSE: -1,
+  DRAW: 0,
+  NOT_FINISHED: -2,
+};
+
+export const checkForWin = (squares, mySymbol) => {
+  const { winner } = findWinningLine(squares) || {};
+
+  if (winner) {
+    if (winner === mySymbol) return WIN_STATE_POINTS.WON;
+    else return WIN_STATE_POINTS.LOOSE;
+  }
+
+  if (squares.every((square) => square)) {
+    return WIN_STATE_POINTS.DRAW;
+  }
+  return WIN_STATE_POINTS.NOT_FINISHED;
+};
